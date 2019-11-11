c0e8538be48aa4d179f713a5bb72ba8f60852267
claudio <clo.santoro@hotmail.it>
2019-11-11 15:32:32 +0100

    Redirezione su login

--- src/app.js
+++ src/app.js
@@ -9,17 +9,36 @@ app.use(bodyParser.json());
 
 const {Pool,Client} = require('pg')
 
+/*******************COGNITO********************/
+
+
+global.fetch = require('node-fetch');
+var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const AWS = require('aws-sdk');
+
+var poolData = { UserPoolId : 'us-east-1_WLaeGxYQd',
+    ClientId : '4d76fpsdlldt729bkjufbj7gcb'
+};
+
+var userPool = new CognitoUserPool(poolData);
+
+
+
+/*******************************************************/
+
 
 const pool = new Pool({
     user:"postgres",
-    host:"sdccdb.cq0cmm7hsjuw.eu-central-1.rds.amazonaws.com",
-    database:"classdb",
+    host:"database-1.cv1l7z2qnpv7.us-east-1.rds.amazonaws.com",
+    //host:"sdccdb.cq0cmm7hsjuw.eu-central-1.rds.amazonaws.com",
+    database:"sdccdb",
     password:"postgres",
     port:5432
 });
 
 const creation = 'CREATE TABLE IF NOT EXISTS "users"(' +
-    'username VARCHAR(10) PRIMARY KEY,' +
+    'username VARCHAR(50) PRIMARY KEY,' +
     'email VARCHAR(50) NOT NULL,' +
     'password VARCHAR(10))';
 
@@ -48,6 +67,79 @@ app.get("/api/u1/users", async (req, res) => {
     const videos = await Video.find({});
     res.json(videos);
 });*/
+function cognitoLog(email, psw) {
+    var idToken;
+    var accessToken;
+
+    console.log('cognitoLogin!');
+    var authenticationData = {
+        Username : 'prova@example.com',// 'closantoro@gmail.com',
+        Password : 'password',
+    };
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+    var poolData = { UserPoolId : 'us-east-1_WLaeGxYQd',
+        ClientId : '4d76fpsdlldt729bkjufbj7gcb'
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    var userData = {
+        Username : 'prova@example.com',//'closantoro@gmail.com',
+        Pool : userPool
+    };
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    console.log('cognitoUser is: ' + JSON.stringify(cognitoUser));
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            accessToken = result.getAccessToken().getJwtToken();
+
+            /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+            idToken = result.idToken.jwtToken;
+
+            console.log('Access token: ' + JSON.stringify(accessToken));
+            console.log('Id token: ' + JSON.stringify(idToken));
+
+            sessionStorage.setItem('accessToken',accessToken);
+            sessionStorage.setItem('idToken',idToken);
+        },
+
+        onFailure: function(err) {
+            console.log('There was an error: ' + JSON.stringify(err));
+            //alert(err);
+        },
+
+    });
+}
+
+
+
+function cognitoSignUp(email,psw) {
+
+    var attribute = {
+        Name : 'email',
+        Value : email
+    };
+    var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(attribute);
+    var attributeList = [];
+
+    attributeList.push(attributeEmail);
+    var cognitoUser;
+
+
+    console.log('trying to signup; userPool is: '+ JSON.stringify(userPool));
+
+    userPool.signUp(email, psw, attributeList, null, function(err, result) {
+        if (err) {
+            console.log('There was an error: ' + JSON.stringify(err));
+            //alert(err);
+            return;
+        }
+        cognitoUser = result.user;
+    });
+
+
+
+
+}
+
 
 app.post("/users/registration", async (req, res) => {
     console.log('called');
@@ -58,11 +150,7 @@ app.post("/users/registration", async (req, res) => {
     console.log('got name: ' + user);
     console.log('got email: ' + email);
     console.log('got psw: ' + psw);
-    //const video = new User({ name: req.body.name });
-    //const savedVideo = await video.save();
-    //res.json(savedVideo);
 
-    //console.log('pool is:  ' + JSON.stringify(pool));
 
     const text = "INSERT INTO users(username, email,password) VALUES($1, $2,$3) RETURNING *";
     const values = [user,email,psw];
@@ -73,7 +161,8 @@ app.post("/users/registration", async (req, res) => {
             console.log("error: "  + err);
         } else {
             console.log("not error in creation");
-            console.log(res.rows[0])
+            cognitoSignUp(email,psw)
+            //console.log(res.rows[0])
             // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
         }
     });
@@ -84,23 +173,26 @@ app.post("/users/registration", async (req, res) => {
 
 app.post("/users/login", (req, res) => {
 
-    let email= req.body.email;
-    let psw = req.body.psw;
+    console.log('body is: ' + JSON.stringify(req.body));
+    let email= req.body.em;
+    let psw = req.body.pass;
     console.log('got email: ' + email);
     console.log('got psw: ' + psw);
 
-    const text = "SELECT FROM users(email,password) VALUES($1, $2) RETURNING *";
+    //const text = "SELECT FROM users(email,password) VALUES($1, $2)";//" RETURNING *";
     const values = [email,psw];
 
-    pool.query(text,values,(err, res) => {
-        console.log('query LOGIN');
+    const text = "SELECT * FROM users WHERE  email ='"+email + "' AND password = '" + psw + "';";
+    pool.query(text,/*values,*/(err, res) => {
+        console.log('query LOGIN: ' + text);
         if (err) {
             console.log("error: "  + err);
             res.json("no logged in");
         } else {
             console.log("not error in creation ");
-            console.log(res.rows[0]);
-            res.json("logged in");
+            //console.log(res.rows[0]);
+            //res.json("logged in");
+            cognitoLog(email,psw);
             // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
         }
     });
--- src/cognito/auth.js
+++ src/cognito/auth.js
@@ -1,12 +1,13 @@
 //var AWS = require('aws-sdk');
 //var cognitoIdentity = new AWS.CognitoIdentity();
 //var CognitoUserPool = cognitoIdentity.CognitoUserPool;
+global.fetch = require('node-fetch');
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 const AWS = require('aws-sdk');
 
-var poolData = { UserPoolId : 'us-east-1_HhEqUyXU9',
-    ClientId : '1hetfr9fr5t34f9jeqr6v593fn'
+var poolData = { UserPoolId : 'us-east-1_WLaeGxYQd',
+    ClientId : '4d76fpsdlldt729bkjufbj7gcb'
 };
 
 var userPool = new CognitoUserPool(poolData);
@@ -15,7 +16,7 @@ console.log('setting attributes');
 
 var attribute = {
     Name : 'email',
-    Value : 'santoroclaudio96@gmail.com'
+    Value : 'closantoro@gmail.com'
 };
 var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(attribute);
 var attributeList = [];
@@ -23,18 +24,64 @@ var attributeList = [];
 attributeList.push(attributeEmail);
 
 
-console.log('pushed');
+/*console.log('pushed');
 
 var cognitoUser;
 
 
-console.log('trying to signup')
+console.log('trying to signup; userPool is: '+ JSON.stringify(userPool));
 
-userPool.signUp('username', 'password', attributeList, null, function(err, result) {
+userPool.signUp('closantoro@gmail.com', 'password', attributeList, null, function(err, result) {
     if (err) {
-        console.log('There was an error: ' + err);
-        alert(err);
+        console.log('There was an error: ' + JSON.stringify(err));
+        //alert(err);
         return;
     }
     cognitoUser = result.user;
 });
+*/
+// Amazon Cognito creates a session which includes the id, access, and refresh tokens of an authenticated user.
+
+var idToken;
+var accessToken;
+
+var authenticationData = {
+    Username : 'closantoro@gmail.com',
+    Password : 'password',
+};
+var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+var poolData = { UserPoolId : 'us-east-1_WLaeGxYQd',
+    ClientId : '4d76fpsdlldt729bkjufbj7gcb'
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+var userData = {
+    Username : 'closantoro@gmail.com',
+    Pool : userPool
+};
+var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+console.log('cognitoUser is: ' + JSON.stringify(cognitoUser));
+cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+        accessToken = result.getAccessToken().getJwtToken();
+
+        /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer */
+        idToken = result.idToken.jwtToken;
+
+
+
+        console.log('Access token: ' + JSON.stringify(accessToken));
+        console.log('Id token: ' + JSON.stringify(idToken));
+
+        sessionStorage.setItem('accessToken',accessToken);
+        sessionStorage.setItem('idToken',idToken);
+    },
+
+    onFailure: function(err) {
+        console.log('There was an error: ' + JSON.stringify(err));
+        //alert(err);
+    },
+
+});
+
+console.log('Access token: ' + JSON.stringify(accessToken));
+console.log('Id token: ' + JSON.stringify(idToken));
